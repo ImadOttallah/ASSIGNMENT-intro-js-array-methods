51ee40e78852837bfed920782fd4f896861709c6
Imad Ottallah <imadotta@gmail.com>
2022-04-28 21:55:39 -0500

    started the assignment

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,11 @@ import { renderToDom } from "../utils/renderToDom.js";
 // Reusable function to get the cards on the DOM
 // .forEach()
 const renderCards = (array) => {
-  let refStuff = "<h1 class='text-white'>Cards Go Here!</h1>";
+  let refStuff = "";
+
+  array.forEach((item) => {
+ refStuff += card(item);
+  });
   renderToDom("#cards", refStuff);
 }
 
@@ -21,28 +25,35 @@ const toggleCart = (event) => {
 // SEARCH
 // .filter()
 const search = (event) => {
-  const eventLC = event.target.value.toLowerCase();
-  console.log(eventLC)
+  console.log(event);
+  const userInput = event.target.value.toLowerCase();
+   const searchResult = referenceList.filter(item => 
+    item.title.toLowerCase().includes(userInput) ||
+    item.author.toLowerCase().includes(userInput) ||
+    item.description.toLowerCase().includes(userInput)
+    )
 }
 
 // BUTTON FILTER
 // .filter() & .reduce() &.sort() - chaining
 const buttonFilter = (event) => {
   if(event.target.id.includes('free')) {
-    console.log('FREE')
+    const free = referenceList.filter(item => item.price <= 0);
+    renderCards(free);
   }
   if(event.target.id.includes('cartFilter')) {
-    console.log('cartFilter')
+    const cart = referenceList.filter(item => item.inCart);
+    renderCards(cart)
   }
   if(event.target.id.includes('books')) {
-    console.log('books!')
+    const books = referenceList.filter(item => item.type.toLowerCase() === "book");
+    renderCards(books)
   }
   if(event.target.id.includes('clearFilter')) {
-    console.log('clearFilter')
+    renderCards(referenceList)
+    
   }
-  if(event.target.id.includes('cartList')) {
-    cartList();
-  }
+
   if(event.target.id.includes('productList')) {
     let table = `<table class="table table-dark table-striped" style="width: 600px">
     <thead>
@@ -76,7 +87,11 @@ const cartTotal = () => {
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
 const productList = () => {
-  return [{ title: "SAMPLE TITLE", price: 45.00, type: "SAMPLE TYPE" }]
+  return referenceList.map(item  => ({
+    title: item.title,
+    price: item.price,
+    type: item.type
+  }))
 }
 
 
